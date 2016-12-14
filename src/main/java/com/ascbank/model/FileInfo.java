9cf264b73e5bd3dcd2c0119cf07730524861a1a8
unjie <446321091@qq.com>
2016-12-14 11:32:07 +0800

    添加AMPQ组件 使用Spring AMPQ jar 包

--- src/main/java/com/ascbank/model/FileInfo.java
+++ src/main/java/com/ascbank/model/FileInfo.java
@@ -1,10 +1,5 @@
 package com.ascbank.model;
 
-import java.io.File;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.attribute.BasicFileAttributes;
 import java.util.Date;
 
 import org.slf4j.Logger;
--- src/main/java/com/ascbank/util/FileUtilsIntensify.java
+++ src/main/java/com/ascbank/util/FileUtilsIntensify.java
@@ -8,14 +8,12 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import java.util.zip.ZipOutputStream;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.IOUtils;
-import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
